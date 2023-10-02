63442bf90f993689a729e56a2c8f74a7744314cc
Talha Shaikh <m-talha-shaikh@protonmail.com>
2023-10-02 23:30:50 +0500

    fixed certificationsQuery

---
+++ server/utils/executeQuery.js
@@ -0,0 +1,12 @@
+function executeQuery(db, query, params) {
+  return new Promise((resolve, reject) => {
+    db.query(query, params, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  })};
+
+module.exports = executeQuery;
