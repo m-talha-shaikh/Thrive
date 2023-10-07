b648dd1513fe3b90305ebb55d8d794001b33a173
Talha Shaikh <m-talha-shaikh@protonmail.com>
2023-10-07 12:38:00 +0500

    added another origin in cors

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ const app = express();
 
 //Middlewares
 app.use(cors({
-    origin: "http://127.0.0.1:5173",
+    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
 }));
+
 
 app.use(helmet());
 app.use(express.json())
